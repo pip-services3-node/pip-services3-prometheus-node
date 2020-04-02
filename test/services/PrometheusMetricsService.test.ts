defcef5ce3d9dec0ee669cd0741f4d8b503bbbe4
Alan Joyce <a.joyce@kyrio.com>
2020-04-02 14:57:03 -0400

    A new method called 'metricsandreset' has been added. This method will reset the counters after they have been read. Unit tests have been added for this change.

--- test/services/PrometheusMetricsService.test.ts
+++ test/services/PrometheusMetricsService.test.ts
@@ -1,11 +1,12 @@
 let assert = require('chai').assert;
-let restify = require('restify');
+let restify = require('restify-clients');
 let async = require('async');
 
 import { Descriptor } from 'pip-services3-commons-node';
 import { ConfigParams } from 'pip-services3-commons-node';
 import { References } from 'pip-services3-commons-node';
 import { ContextInfo } from 'pip-services3-components-node';
+import { CounterType } from 'pip-services3-components-node';
 
 import { PrometheusMetricsService } from '../../src/services/PrometheusMetricsService';
 import { PrometheusCounters } from '../../src/count/PrometheusCounters';
@@ -16,7 +17,7 @@ var restConfig = ConfigParams.fromTuples(
     "connection.port", 3000
 );
 
-suite('PrometheusMetricsService', ()=> {
+suite('PrometheusMetricsService', () => {
     let service: PrometheusMetricsService;
     let counters: PrometheusCounters;
     let rest: any;
@@ -43,7 +44,7 @@ suite('PrometheusMetricsService', ()=> {
             service.open(null, done);
         });
     });
-    
+
     suiteTeardown((done) => {
         service.close(null, (err) => {
             counters.close(null, done);
@@ -54,7 +55,7 @@ suite('PrometheusMetricsService', ()=> {
         let url = 'http://localhost:3000';
         rest = restify.createStringClient({ url: url });
     });
-    
+
     test('Metrics', (done) => {
         counters.incrementOne('test.counter1');
         counters.stats('test.counter2', 2);
@@ -64,7 +65,7 @@ suite('PrometheusMetricsService', ()=> {
         rest.get('/metrics',
             (err, req, res, result) => {
                 assert.isNull(err);
-                
+
                 assert.isNotNull(result);
                 assert.isTrue(res.statusCode < 400);
                 assert.isTrue(result.length > 0);
@@ -74,4 +75,32 @@ suite('PrometheusMetricsService', ()=> {
         );
     });
 
+    test('MetricsAndReset', (done) => {
+        counters.incrementOne('test.counter1');
+        counters.stats('test.counter2', 2);
+        counters.last('test.counter3', 3);
+        counters.timestampNow('test.counter4');
+
+        rest.get('/metricsandreset',
+            (err, req, res, result) => {
+                assert.isNull(err);
+
+                assert.isNotNull(result);
+                assert.isTrue(res.statusCode < 400);
+                assert.isTrue(result.length > 0);
+
+                let counter1 = counters.get("test.counter1", CounterType.Increment);
+                let counter2 = counters.get("test.counter2", CounterType.Statistics);
+                let counter3 = counters.get("test.counter3", CounterType.LastValue);
+                let counter4 = counters.get("test.counter4", CounterType.Timestamp);
+
+                assert.isUndefined(counter1.count);
+                assert.isUndefined(counter2.count);
+                assert.isUndefined(counter3.last);
+                assert.isUndefined(counter4.time);
+
+                done();
+            }
+        );
+    });
 });
